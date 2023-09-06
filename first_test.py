fbb4a6406cc7a184b835e1e7a3473a722223ae8f
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-09-06 15:38:37 +0300

    refactoring

--- first_test.py
+++ first_test.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome()
-browser.get("http://suninjuly.github.io/simple_form_find_task.html")
-button = browser.find_element(By.ID, "submit_button")
-button.click()
-browser.quit()
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.ID, "submit_button")
+    button.click()
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
