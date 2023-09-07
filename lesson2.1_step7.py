3a439be638d51fb1bca3215125662802dffc9e7a
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-09-07 23:00:25 +0300

    add checkbox clicker with search in img attributes

---
+++ lesson2.1_step7.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math, time
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try: 
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#treasure")
+    x = x_element.get_attribute('valuex')
+    y = calc(x)
+
+    checkbox = browser.find_element(By.CSS_SELECTOR, "#answer")
+    checkbox.send_keys(y)
+
+    checkbox = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    checkbox.click()
+
+    checkbox = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    checkbox.click()
+
+    checkbox = browser.find_element(By.CSS_SELECTOR, ".btn-default")
+    checkbox.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
