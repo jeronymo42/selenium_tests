fc5c53eb35e3ac67474f5fa7f02ccd5371a3bf84
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-09-08 00:40:26 +0300

    add selector test

---
+++ lesson2.2_step2.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select 
+import time
+
+try: 
+    link = "https://suninjuly.github.io/selects1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    num1 = browser.find_element(By.CSS_SELECTOR, "#num1").text
+    num2 = browser.find_element(By.CSS_SELECTOR, "#num2").text
+
+    select = Select(browser.find_element(By.TAG_NAME, "select"))
+    select.select_by_value(str(int(num1)+int(num2)))
+
+    browser.find_element(By.CSS_SELECTOR, ".btn").click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
