2f48b40ea1088f34ac15d61a8dc43922969d3e38
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-09-08 01:20:05 +0300

    add test with scrolling page

---
+++ lesson2.2_step6.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import math
+import time
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "https://SunInJuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    num = browser.find_element(By.CSS_SELECTOR, "#input_value").text
+    result = calc(num)
+    field = browser.find_element(By.CSS_SELECTOR, "#answer").send_keys(result)
+
+    checkbox = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox").click()
+
+    button = browser.find_element(By.TAG_NAME, "button")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    
+    radio = browser.find_element(By.CSS_SELECTOR, "#robotsRule").click()
+    button.click()
+    
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
